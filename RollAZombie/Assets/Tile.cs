2d8db2ebd16edcf27a05e2ac585709eb17500351
sayali-deshpande <sjk126@gmail.com>
2019-03-23 11:56:16 +0530

    Intial commit

---
+++ RollAZombie/Assets/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameManager gameManager;
+    public AudioClip hit;
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        source.PlayOneShot(hit);
+        gameManager.AddPoint();
+    }
+}
